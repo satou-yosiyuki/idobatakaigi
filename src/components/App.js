a2f219986fa13270fa92af525fed973be9e5ac30
satou-yosiyuki <stuysyk1027@icloud.com>
2021-06-12 22:29:02 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Main from "./Main";
-
+import config from "../config.json";
 import SignIn from "./SignIn";
+
 // setNameにpropsでsetName関数を渡してる
 export default () => {
   const [name, setName] = useState("");
   console.log({ name });
-  // 空文字列の時にSignInの画面,文字が入ったら、Mainの画面  
-  if (name === "") {
+  // 空文字列の時にSignInの画面,文字が入ったら、Mainの画面
+  if (config.signInEnabled && name === "") {
     return <SignIn setName={setName} />;
   }
-  return <Main name={name}/>
+  return <Main name={name} />;
 };

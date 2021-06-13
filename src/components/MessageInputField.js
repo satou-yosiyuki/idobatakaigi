c345d25fd4648c8a15fdbfc92f6a746db6cb138e
satou-yosiyuki <stuysyk1027@icloud.com>
2021-06-13 09:48:01 +0900

    avatar generate

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Avatar, Grid } from "@material-ui/core";
+import {gravatarPath} from '../gravatar';
 // 度の線にぶら下がるのか
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
-    margin : '26px'
+    margin: "26px",
   },
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({ name }) => {
   const classes = useStyles();
+  const avatarPath=gravatarPath(name);
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid xs={1}>
-          <Avatar />
+          <Avatar src={avatarPath}/>
         </Grid>
         <Grid xs={10}>入力</Grid>
         <Grid xs={1}>ボタン</Grid>
